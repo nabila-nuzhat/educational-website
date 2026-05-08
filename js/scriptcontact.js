be693f93f5e3816c68334895e5d0c2ba63834d89
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-05-09 00:36:28 +0600

    contact form validation and date and time js added

--- js/scriptcontact.js
+++ js/scriptcontact.js
@@ -1,12 +1,87 @@
-function showDateTime(){
-    const now = new Date();
+// function showDateTime(){
+//     const now = new Date();
 
-    const showOnPage = now.toLocaleString();
+//     const showOnPage = now.toLocaleString();
 
-    document.querySelector("#today").textContent = showOnPage;
-    console.log(document.querySelector("#today").textContent);
-    
+//     document.querySelector("#today").textContent = showOnPage;
+//     console.log(document.querySelector("#today").textContent);
+
+// }
+
+// showDateTime();
+// setInterval(showDateTime, 1000);
+
+/* for validation contact --------------*/
+
+// name not a number check
+function testNumberinName(text) {
+    for (char of text) {
+        if (!isNaN(char) && char !== " ") {
+            return true;
+        }
+        return false;
+    }
 }
 
-showDateTime();
-setInterval(showDateTime, 1000);
+function formValidation() {
+    const name = document.getElementById("fullname").value;
+    // console.log(name);
+    const email = document.getElementById("exampleInputEmail1").value;
+    const ph = document.getElementById("ph").value;
+    const pass = document.getElementById("exampleInputPassword1").value;
+
+    // error msg
+
+    const errorName = document.getElementById("errorName");
+    const errorEmail = document.getElementById("errorEmail");
+    const errorPh = document.getElementById("errorPh");
+    const errorPass = document.getElementById("errorPass");
+
+    // initial error content text blank
+    errorName.textContent = "";
+    errorEmail.textContent = "";
+    errorPh.textContent = "";
+    errorPass.textContent = "";
+
+    // initially
+    let isValid = true;
+
+    if (name === "" || testNumberinName(name)) {
+        errorName.textContent = "Please enter valid name";
+        // alert("Please enter valid name");
+        isValid = false;
+    }
+    if (email === "" || !email.includes("@") || !email.includes(".")) {
+        errorEmail.textContent = "Please enter valid email";
+        isValid = false;
+    }
+    if (ph === "" || isNaN(ph)) {
+        errorPh.textContent = "Please enter ph number";
+        isValid = false;
+    }
+    if (pass === "") {
+        errorPass.textContent = "Please enter password";
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Submitted Successfully");
+        // return true;
+    } else {
+        return false;
+    }
+}
+
+
+    function resetErrors() {
+      document.getElementById("name-error").textContent     = "";
+      document.getElementById("address-error").textContent  = "";
+      document.getElementById("email-error").textContent    = "";
+      document.getElementById("password-error").textContent = "";
+      document.getElementById("subject-error").textContent  = "";
+      document.getElementById("agree-error").textContent    = "";
+    }
+
+
+
+    
